ba41c78289e97e755fafd99366d54610e380623b
Matthew Herman <mattherman11@gmail.com>
2020-12-12 18:23:56 -0600

    Added links so I don't forget

--- src/GiftWrapping.fs
+++ src/GiftWrapping.fs
@@ -2,6 +2,8 @@ module GiftWrapping
 
 open Vector
 
+// https://www.geeksforgeeks.org/direction-point-line-segment/
+// https://math.stackexchange.com/questions/1435779/calculate-if-a-point-lies-above-or-below-or-right-to-left-of-a-line
 // The cross product of two vectors, A X B, is positive if the angle from A -> B is counter-clockwise.
 // If A = (ax, ay) and B = (bx, by), then A X B = (ax * by) - (ay * bx).
 // However, this assumes A and B are vectors starting at the origin (0, 0). If this is not the case, we
