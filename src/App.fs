9b7390cd3c0448f42326c9465484185e8ee6a456
Matthew Herman <mattherman11@gmail.com>
2020-12-13 21:18:50 -0600

    Resize canvas

--- src/App.fs
+++ src/App.fs
@@ -10,8 +10,8 @@ open GiftWrapping
 
 let rnd = Random()
 
-let height = 800.
-let width = 800.
+let height = 400.
+let width = 400.
 let numberOfPoints = 50
 
 let canvas = document.getElementById "canvas" :?> HTMLCanvasElement 
@@ -21,8 +21,8 @@ let ctx = canvas.getContext_2d()
 let getRandomPoints (): Point list =
     List.init numberOfPoints (fun _ ->
         // Limit range of random values to the center of the canvas
-        let x = int (0.25 * width) + (rnd.Next (int (0.5 * width)))
-        let y = int (0.25 * height) + (rnd.Next (int (0.5 * height)))
+        let x = int (0.1 * width) + (rnd.Next (int (0.8 * width)))
+        let y = int (0.1 * height) + (rnd.Next (int (0.8 * height)))
         { X = x; Y = y })
 
 let mutable points = getRandomPoints ()
